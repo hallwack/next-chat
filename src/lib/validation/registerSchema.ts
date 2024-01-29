a15617f62a37e1661852bc9180cb93eabd4ba116
Raihan Adam <hallwack.id@gmail.com>
2024-01-29 15:28:53 +0700

    feat: create validation schema for auth

---
+++ src/lib/validation/registerSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const registerSchema = z
+  .object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+    confirmPassword: z.string().min(6),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Password does not match",
+  });
