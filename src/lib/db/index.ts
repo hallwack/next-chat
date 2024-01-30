74134d7587b960de063cde6719479c86e812b028
Raihan Adam <hallwack.id@gmail.com>
2024-01-30 08:49:47 +0700

    fix: move migrate file

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,21 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import * as schema from "./schema";
 
 const connectionString = process.env.DATABASE_URL as string;
 const client = postgres(connectionString, { prepare: false });
 const db = drizzle(client, { schema });
-const migrateDb = async () => {
-  try {
-    console.log("🟠 Migrating client");
-    await migrate(db, { migrationsFolder: "migrations" });
-    console.log("🟢 Successfully Migrated");
-  } catch (error) {
-    console.log("🔴 Error Migrating client");
-  }
-};
 
-migrateDb();
-
-export default { db, connectionString };
+export default db;
