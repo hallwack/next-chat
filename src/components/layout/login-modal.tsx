a5ac33cfbb788ca182ceaef617efec3b8e85e3ae
Raihan Adam <hallwack.id@gmail.com>
2024-02-01 09:36:16 +0700

    feat: create login and register modal

---
+++ src/components/layout/login-modal.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import LoginForm from "../shared/login-form";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+
+export default function LoginModal() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <>
+      <Dialog
+        defaultOpen
+        /* open={pathname.includes("/login")} */
+        onOpenChange={() => router.back()}
+      >
+        <DialogContent className="max-w-80">
+          <DialogHeader>
+            <DialogTitle>Login</DialogTitle>
+          </DialogHeader>
+          <LoginForm />
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
